b4bcf00c48186aad9c005e899697e40463498e8b
Mark-Hodge <33436232+Mark-Hodge@users.noreply.github.com>
2019-11-06 17:22:27 -0600

    Added Local Storage

---
+++ main/java/com/google/firebase/codelab/friendlychat/DatabaseManager.java
@@ -0,0 +1,158 @@
+package com.example.chat_application_alpha;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import com.example.chat_application_alpha.Model.Chat;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+
+public class DatabaseManager extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 10;
+    private static final String DATABASE_NAME = "local_backup.db";
+
+    // TableID and columns for messages table
+    private static final String TABLE_MESSAGE = "messages";
+    private static final String COLUMN_MESSAGEID = "msg_id";
+    private static final String COLUMN_SENDER = "sender";
+    private static final String COLUMN_RECEIVER = "receiver";
+    private static final String COLUMN_MESSAGE = "message";
+    private static final String COLUMN_SYMKEY = "symetric_key";
+
+    // TableID and columns for account table
+    private static final String TABLE_ACCOUNT = "account";
+    private static final String COLUMN_ACCOUNTID = "account_id";
+    private static final String COLUMN_FIREBASEID = "firebase_id";
+    private static final String COLUMN_USERNAME = "username";
+    private static final String COLUMN_EMAIL = "email";
+    private static final String COLUMN_PASSWORD = "password";
+    private static final String COLUMN_PVTKEY = "private_key";
+
+    public DatabaseManager(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, DATABASE_NAME, factory, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String query = "CREATE TABLE " + TABLE_MESSAGE + "("
+                + COLUMN_MESSAGEID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + COLUMN_SENDER + " TEXT NOT NULL, "
+                + COLUMN_RECEIVER + " TEXT NOT NULL, "
+                + COLUMN_MESSAGE + " TEXT NOT NULL, "
+                + COLUMN_SYMKEY + " TEXT NOT NULL" + ");";
+        db.execSQL(query);
+
+        query = new String();
+        query = "CREATE TABLE " + TABLE_ACCOUNT + "("
+                + COLUMN_ACCOUNTID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + COLUMN_FIREBASEID + " TEXT NOT NULL, "
+                + COLUMN_USERNAME + " TEXT NOT NULL, "
+                + COLUMN_EMAIL + " TEXT NOT NULL, "
+                + COLUMN_PASSWORD + " TEXT NOT NULL, "
+                + COLUMN_PVTKEY + " TEXT NOT NULL" + ");";
+        db.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_MESSAGE);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_ACCOUNT);
+        onCreate(db);
+    }
+
+    public void insertMessage(String sender, String receiver, String message, String key) { //TODO: change to add row into message table
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_SENDER, sender);
+        values.put(COLUMN_RECEIVER, receiver);
+        values.put(COLUMN_MESSAGE, message);
+        values.put(COLUMN_SYMKEY, key);
+
+
+        SQLiteDatabase db = getWritableDatabase();
+        String query = "SELECT e.*, MAX(m.'msg_id') "
+                        + "FROM " + TABLE_MESSAGE + " e, " + TABLE_MESSAGE + " m "
+                        + "WHERE e.'" + COLUMN_MESSAGEID + "' = m.'" + COLUMN_MESSAGEID + "'";
+
+        Cursor cursor = db.rawQuery(query, null);
+        cursor.moveToFirst();
+
+        int maxID = cursor.getInt(cursor.getColumnIndex(COLUMN_MESSAGEID));
+        String existingSender = cursor.getString(cursor.getColumnIndex(COLUMN_SENDER));
+        String existingReceiver = cursor.getString(cursor.getColumnIndex(COLUMN_RECEIVER));
+        String existingMessage = cursor.getString(cursor.getColumnIndex(COLUMN_MESSAGE));
+        String existingKey = cursor.getString(cursor.getColumnIndex(COLUMN_SYMKEY));
+
+        if (existingSender.equals(sender) && existingReceiver.equals(receiver) &&
+                existingMessage.equals(message) && existingKey.equals(key)) {
+            db.close();
+
+        } else {
+            db.insert(TABLE_MESSAGE, null, values);
+        }
+
+        db.close();
+    }
+
+    public void insertAccount(String firebaseID, String username, String email, String password, String privateKey) { //TODO: Add inserts for account
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_FIREBASEID, firebaseID);
+        values.put(COLUMN_USERNAME, username);
+        values.put(COLUMN_EMAIL, email);
+        values.put(COLUMN_PASSWORD, password);
+        values.put(COLUMN_PVTKEY, privateKey);
+
+        SQLiteDatabase db = getWritableDatabase();
+        db.insert(TABLE_ACCOUNT, null, values);
+        db.close();
+    }
+
+    public String retreivePrivateKey(String firebaseID) {
+        String dbString = "";
+        SQLiteDatabase db = getWritableDatabase();
+        String query = "SELECT " + COLUMN_PVTKEY + " FROM " + TABLE_ACCOUNT + " WHERE " + COLUMN_FIREBASEID + "='" + firebaseID + "'";
+        Cursor cursor = db.rawQuery(query, null);
+        cursor.moveToFirst();
+        dbString += cursor.getString(cursor.getColumnIndex("private_key"));
+
+        db.close();
+        return dbString;
+    }
+
+    public String databaseToString(Chat chat) {
+        String dbString = "";
+        SQLiteDatabase db = getWritableDatabase();
+        String query = "SELECT * FROM " + TABLE_MESSAGE + " WHERE " + COLUMN_SENDER + "='" + chat.getSender() + "'"
+                + " AND " + COLUMN_RECEIVER + "='" + chat.getReceiver() + "'" + " AND " + COLUMN_MESSAGE + "='" + chat.getMessage() + "'";
+        Cursor cursor = db.rawQuery(query, null);
+        cursor.moveToFirst();
+
+        dbString += cursor.getString(cursor.getColumnIndex("message"));
+
+//        while (!cursor.isAfterLast()) {
+//            if (cursor.getString(cursor.getColumnIndex("message"))!= null) {
+//                dbString += cursor.getString(cursor.getColumnIndex("message"));
+//                dbString += "\n";
+//            }
+//        }
+        db.close();
+        return dbString;
+    }
+}
+
+
+
+
+
+
+
+
+
+
